6a16c935b4475458e310fb037bfe3ab2beb036a5
yaroslavboreyko <yaroslav.boreyko@gmail.com>
2022-06-06 10:20:25 +0200

    Nest-Next configurations start

---
+++ src/server/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Get()
+  @Render('index')
+  home() {
+    return {};
+  }
+}
